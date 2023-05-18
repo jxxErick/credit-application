0793a10b7f1ea1bb5bce3384b285b0a930ffbe56
jxxErick <ericksdantas144@gmail.com>
2023-05-17 22:49:11 -0300

    First commit

---
+++ src/test/kotlin/com/jxxe/creditrequestsystem/CreditRequestSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jxxe.creditrequestsystem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CreditRequestSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
