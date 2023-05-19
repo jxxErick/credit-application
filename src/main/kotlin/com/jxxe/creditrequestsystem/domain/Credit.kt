14cc3f56648019028a352d36e8044dc771fe2055
jxxErick <ericksdantas144@gmail.com>
2023-05-19 01:54:38 -0300

    Finshed service class

--- src/main/kotlin/com/jxxe/creditrequestsystem/domain/Credit.kt
+++ src/main/kotlin/com/jxxe/creditrequestsystem/domain/Credit.kt
@@ -2,6 +2,7 @@ package com.jxxe.creditrequestsystem.domain
 
 import com.jxxe.creditrequestsystem.ennumeration.Status
 import jakarta.persistence.*
+import org.hibernate.Hibernate
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.*
@@ -14,6 +15,21 @@ data class Credit (
    @Column (nullable = false)  val dayFisrtInstallment: LocalDate,
    @Column (nullable = false)   val numberOfInstallments: Int = 0,
     @Enumerated val status: Status = Status.IN_PROGRESS,
-    @ManyToOne  val customer: Customer? = null,
+    @ManyToOne  var customer: Customer? = null,
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as Credit
+
+        return id != null && id == other.id
+    }
+
+    override fun hashCode(): Int = javaClass.hashCode()
+
+    @Override
+    override fun toString(): String {
+        return this::class.simpleName + "(id = $id )"
+    }
+}
