fe3848f8ad0b93ec90212a69e122451e44b7b4c6
jxxErick <ericksdantas144@gmail.com>
2023-05-23 15:24:24 -0300

    test service 100%

---
+++ src/test/kotlin/com/jxxe/creditrequestsystem/repository/CreditRepositoryTest.kt
@@ -0,0 +1,19 @@
+package com.jxxe.creditrequestsystem.repository
+
+import com.jxxe.creditrequestsystem.repositories.CustomerRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CreditRepositoryTest {
+@Autowired lateinit var customerRepository: CustomerRepository
+@Autowired lateinit var testEntityManager: TestEntityManager
+
+
+
+}
